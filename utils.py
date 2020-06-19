e66e66439438d18622a0a5b4d6123626b22e1ee6
LeoCovarrubias <leonardo1garciac@gmail.com>
2020-06-19 11:21:19 -0500

    Upload Utilities

---
+++ utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 06:55:06 2020
+@author: lcovarrubias, eledgarmurillo
+
+"""
+import numpy as numpy
+numpy.random
+
+def rand_pm():
+    if numpy.random.rand()<0.5:
+        return -1
+    else:
+        return 1
+
+def bool_pm(b):
+    if b:
+        return 1
+    else:
+        return -1
+
+def w2s(a):
+    #e.g., [-1,1,-1] -> 010 -> 2
+    s = 0
+    p = len(a)-1
+    for i in a:
+        if i == 1:
+            s += 2**p
+        p -= 1
+    return s
+
+def minority(d):
+    if d<0:
+        return 1
+    elif d>0:
+        return -1
+    else:
+        return rand_pm()
+            
+def max_idx(a):
+    L = len(a)
+    idx =[0]
+    v = a[0]
+    for i in range(1, L):
+        if a[i] > v:
+            idx = [i]
+            v = a[i]
+        elif a[i] == v:
+            idx.append(i)
+        else:
+            continue
+    i = numpy.random.randint(len(idx))
+    return idx[i]
